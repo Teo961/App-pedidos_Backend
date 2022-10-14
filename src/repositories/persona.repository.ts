16945c92944f030c06f70aae6ad4cf94f69bc84f
teo <teodoroavendano7@gmail.com>
2022-10-14 18:19:56 -0500

    version 1.0. de pedidos

---
+++ src/repositories/persona.repository.ts
@@ -0,0 +1,26 @@
+import {inject, Getter} from '@loopback/core';
+import {DefaultCrudRepository, repository, BelongsToAccessor, HasManyRepositoryFactory} from '@loopback/repository';
+import {MongodbDataSource} from '../datasources';
+import {Persona, PersonaRelations, Pedido} from '../models';
+import {PedidoRepository} from './pedido.repository';
+
+export class PersonaRepository extends DefaultCrudRepository<
+  Persona,
+  typeof Persona.prototype.id,
+  PersonaRelations
+> {
+
+  public readonly pedido: BelongsToAccessor<Pedido, typeof Persona.prototype.id>;
+
+  public readonly pedidos: HasManyRepositoryFactory<Pedido, typeof Persona.prototype.id>;
+
+  constructor(
+    @inject('datasources.mongodb') dataSource: MongodbDataSource, @repository.getter('PedidoRepository') protected pedidoRepositoryGetter: Getter<PedidoRepository>,
+  ) {
+    super(Persona, dataSource);
+    this.pedidos = this.createHasManyRepositoryFactoryFor('pedidos', pedidoRepositoryGetter,);
+    this.registerInclusionResolver('pedidos', this.pedidos.inclusionResolver);
+    this.pedido = this.createBelongsToAccessorFor('pedido', pedidoRepositoryGetter,);
+    this.registerInclusionResolver('pedido', this.pedido.inclusionResolver);
+  }
+}
